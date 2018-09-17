d7339c271a1c1b66feec90d655e71066bbfd121b
wuqingfeng <wuqingfeng234@163.com>
2018-09-17 10:41:08 +0800

    zookeeper客户端测试

--- zkacess/src/main/java/com/huawei/wuqf/CuratorClientTest.java
+++ zkacess/src/main/java/com/huawei/wuqf/CuratorClientTest.java
@@ -6,45 +6,52 @@ import org.apache.curator.retry.RetryNTimes;
 
 public class CuratorClientTest {
 
-    /** Zookeeper info */
+    /**
+     * Zookeeper info
+     */
     private static final String ZK_ADDRESS = "localhost:2181";
     private static final String ZK_PATH = "/zktest";
 
     public static void main(String[] args) throws Exception {
-        // 1.Connect to zk
-        CuratorFramework client = CuratorFrameworkFactory.newClient(
-                ZK_ADDRESS,
-                new RetryNTimes(10, 5000)
-        );
-        client.start();
-        System.out.println("zk client start successfully!");
+        CuratorFramework client = null;
+        try {
+            // 1.Connect to zk
+            client = CuratorFrameworkFactory.newClient(
+                    ZK_ADDRESS,
+                    new RetryNTimes(10, 5000)
+            );
+            client.start();
+            System.out.println("zk client start successfully!");
 
-        // 2.Client API test
-        // 2.1 Create node
-        String data1 = "hello";
-        print("create", ZK_PATH, data1);
-        client.create().
-                creatingParentsIfNeeded().
-                forPath(ZK_PATH, data1.getBytes());
+            // 2.Client API test
+            // 2.1 Create node
+            String data1 = "hello";
+            print("create", ZK_PATH, data1);
+            client.create().
+                    creatingParentsIfNeeded().
+                    forPath(ZK_PATH, data1.getBytes());
 
-        // 2.2 Get node and data
-        print("ls", "/");
-        print(client.getChildren().forPath("/"));
-        print("get", ZK_PATH);
-        print(client.getData().forPath(ZK_PATH));
+            // 2.2 Get node and data
+            print("ls", "/");
+            print(client.getChildren().forPath("/"));
+            print("get", ZK_PATH);
+            print(client.getData().forPath(ZK_PATH));
 
-        // 2.3 Modify data
-        String data2 = "world";
-        print("set", ZK_PATH, data2);
-        client.setData().forPath(ZK_PATH, data2.getBytes());
-        print("get", ZK_PATH);
-        print(client.getData().forPath(ZK_PATH));
+            // 2.3 Modify data
+            String data2 = "world";
+            print("set", ZK_PATH, data2);
+            client.setData().forPath(ZK_PATH, data2.getBytes());
+            print("get", ZK_PATH);
+            print(client.getData().forPath(ZK_PATH));
 
-        // 2.4 Remove node
-        print("delete", ZK_PATH);
-        client.delete().forPath(ZK_PATH);
-        print("ls", "/");
-        print(client.getChildren().forPath("/"));
+            // 2.4 Remove node
+            print("delete", ZK_PATH);
+            client.delete().forPath(ZK_PATH);
+            print("ls", "/");
+            print(client.getChildren().forPath("/"));
+        } finally {
+            client.close();
+        }
     }
 
     private static void print(String... cmds) {
--- zkacess/src/main/java/com/huawei/wuqf/CuratorWatcherTest.java
+++ zkacess/src/main/java/com/huawei/wuqf/CuratorWatcherTest.java
@@ -8,26 +8,21 @@ import org.apache.curator.retry.RetryNTimes;
 
 public class CuratorWatcherTest {
 
-    /** Zookeeper info */
+    /**
+     * Zookeeper info
+     */
     private static final String ZK_ADDRESS = "localhost:2181";
     private static final String ZK_PATH = "/zktest";
 
     public static void main(String[] args) throws Exception {
         // 1.Connect to zk
-        CuratorFramework client = CuratorFrameworkFactory.newClient(
-                ZK_ADDRESS,
-                new RetryNTimes(10, 5000)
-        );
+        CuratorFramework client = CuratorFrameworkFactory.newClient(ZK_ADDRESS, new RetryNTimes(10, 5000));
         client.start();
         System.out.println("zk client start successfully!");
 
         // 2.Register watcher
-        PathChildrenCache watcher = new PathChildrenCache(
-                client,
-                ZK_PATH,
-                true    // if cache data
-        );
-        watcher.getListenable().addListener((client1, event) -> {
+        PathChildrenCache watcher = new PathChildrenCache(client, ZK_PATH, true);
+        watcher.getListenable().addListener((c, event) -> {
             ChildData data = event.getData();
             if (data == null) {
                 System.out.println("No data in event[" + event + "]");
